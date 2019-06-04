f04c2f44a7d2d7058a637ca814caac052465d7f4
jordan-coded4 <jordan@coded4.co.uk>
2019-06-04 13:04:58 +0100

    Changes to the replace function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports =function tabulate(tabString) {
 	var tableStart = '<tr><td>';
-	tabString.replace(/(§)/gm, '</td><td>').replace(/(\n)/gm, '</td></tr><tr><td>');
-	return tableStart.concat(tabString)
+	var newString = tabString.replace(/(§)/gm, '</td><td>').replace(/(\n)/gm, '</td></tr><tr><td>');
+	return tableStart.concat(newString)
 }
