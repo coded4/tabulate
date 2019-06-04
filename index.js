84e5df31429452804dcab0f69f95ed0c4efa8cf9
jordan-coded4 <jordan@coded4.co.uk>
2019-06-04 13:26:58 +0100

    Ammendments to replace to see character

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports =function tabulate(tabString) {
 	var tableStart = '<tr><td>';
-	var newString = tabString.replace(/(§)/gm, '</td><td>').replace(/(\n)/gm, '</td></tr><tr><td>');
+	var newString = tabString.replace(/§/gm, '</td><td>').replace(/(\n)/gm, '</td></tr><tr><td>');
 	return tableStart.concat(newString)
 }
